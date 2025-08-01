6e6f9d757bf39f2b3fb2387d1ddea30c42c46cf7
sachinkrsajan <sachinkrsajan@gmail.com>
2025-08-01 15:51:00 +0530

    add new line

--- C1_variable_constant_keywords/01_variable.c
+++ C1_variable_constant_keywords/01_variable.c
@@ -4,6 +4,7 @@ int main(){
     a=6;                         //variable initialisation
     printf("hello world\n");
     printf("The output of the program is %d",a);
+    printf("nice");
     return 0;
 }
 
