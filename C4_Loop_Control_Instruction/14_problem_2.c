b591e2fc318be26de36b0f000f9c7e8bc637cbc7
sachinkrsajan <sachinkrsajan@gmail.com>
2025-08-08 21:55:38 +0530

    3 practice questions of loop

---
+++ C4_Loop_Control_Instruction/14_problem_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n=10;
+    int i = 10;
+    while (i>0){
+
+        printf("%d * %d = %d\n",n,i,n*i);
+        i--;
+    }
+    return 0;
+}
---
+++ C4_Loop_Control_Instruction/14_problem_5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n=0;
+    int i=0;
+    while (i<=10){
+        n=n+i;
+        i++;
+        printf("%d\n",n);
+    }
+    return 0;
+}
