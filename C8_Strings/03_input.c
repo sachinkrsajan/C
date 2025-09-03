895b1febe252daa307d851abb42b9634098f6d72
sachinkrsajan <sachinkrsajan@gmail.com>
2025-09-03 22:30:34 +0530

    ch8 strings progression

---
+++ C8_Strings/03_input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char st[4];
+    // scanf("%s",st); // same as 
+    scanf("%s",&st[0]);
+    printf("%s",st);
+    return 0;
+}
---
+++ C8_Strings/04_gets_puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char st[30];
+    gets (st);
+    // printf("%s",st);
+    puts(st);
+    printf("hey");
+    return 0;
+}
